c0d0ac7ca9e979cee239bf9f93d4bab68edbf28e
yousefdalloul <yosefasaddaloull@gmail.com>
2023-09-28 11:00:29 +0300

    Jobs, Queues and Scheduler

--- app/Listeners/SendNotificationToAssignedStudents.php
+++ app/Listeners/SendNotificationToAssignedStudents.php
@@ -3,6 +3,7 @@
 namespace App\Listeners;
 
 use App\Events\ClassworkCreated;
+use App\Jobs\SendClassroomNotification;
 use App\Notifications\NewClassworkNotification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
@@ -23,11 +24,17 @@ class SendNotificationToAssignedStudents
      */
     public function handle(ClassworkCreated $event): void
     {
+
 //        foreach ($event->classwork->users() as $user){
 //            $user->notify(new NewClassworkNotification($event->classwork));
 //        }
 
-        Notification::send($event->classwork->users ,new NewClassworkNotification($event->classwork));
-
+        $classwork = $event->classwork;
+        $job = new SendClassroomNotification(
+            $classwork->users
+            ,new NewClassworkNotification($classwork));
+        $job->onQueue('notifications');
+        dispatch($job)->onQueue('notifications');
+        //SendClassroomNotification::dispatch($classwork->users ,new NewClassworkNotification($classwork));
     }
 }
--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -6,15 +6,21 @@ use App\Concerns\HasPrice;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
+use DragonCode\Contracts\Http\Builder;
 
 class Subscription extends Model
 {
-    use HasFactory, HasPrice;
+    use HasFactory, HasPrice, Prunable;
 
     protected $fillable = [
         'user_id','plan_id','price','expires_at',
     ];
 
+    public function prunable(): Builder
+    {
+        return static::where('expires_at', '<=', now()->subYear());
+    }
     protected $casts = [
         'expires_at' => 'datetime',
     ];
